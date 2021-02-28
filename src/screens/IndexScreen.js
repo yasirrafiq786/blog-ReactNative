9c4ab3fb3d08aaae68bb00f9a4d3bc5e162d582e
Yasir Malik <yasirrafiq786@gmail.com>
2021-03-01 09:07:40 +1000

    added context

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import BlogContext from '../context/BlogContext';
+
+const IndexScreen = () => {
+  const value = useContext(BlogContext);
+  return (
+    <View>
+      <Text>Index Screen</Text>
+      <Text>{value}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
